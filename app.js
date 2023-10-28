d7fd59bf28736922e960d242dd30c9268188f9ea
KSrikar02 <ksrikar013@gmail.com>
2023-10-28 21:46:30 +0530

    added feature to stop and reset game after 5 wins

--- app.js
+++ app.js
@@ -8,41 +8,39 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-
-function getComputerChoice(){
+function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-//console.log(getComputerChoice());
-
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + "(user)" + " beat " + computerChoice + "(comp)";
-    console.log("round won ");
+    result_p.innerHTML = userChoice + "(user)" + " beats " + computerChoice + "(comp)";
+    if (userScore === 5) {
+        alert("User wins! Restarting the game.");
+        resetGame();
+    }
 }
 
 function lose(userChoice, computerChoice) {
     computerScore++;
-    userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + "(user)" + " lost to  " + computerChoice + "(comp)" ;
-    console.log("round lost");
+    result_p.innerHTML = userChoice + "(user)" + " loses to " + computerChoice + "(comp)";
+    if (computerScore === 5) {
+        alert("Computer wins! Restarting the game.");
+        resetGame();
+    }
 }
 
 function tie(userChoice, computerChoice) {
-    result_p.innerHTML = userChoice + "(user)" + " and the   " + computerChoice + "(comp)" + "  drew  .";
-    console.log("it's a draw ");
+    result_p.innerHTML = userChoice + "(user)" + " and the " + computerChoice + "(comp)" + " drew.";
 }
-
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) 
-    {
+    switch (userChoice + computerChoice) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
@@ -59,27 +57,32 @@ function game(userChoice) {
             tie(userChoice, computerChoice);
             break;
     }
-    }
+}
 
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = "Let's play!";
+}
 
 function main() {
-rock_div.addEventListener('click', function() {
-    game("rock");
-    
-})
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
 
-paper_div.addEventListener('click', function() {
-    game("paper");
-    
-})
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
 
-scissors_div.addEventListener('click', function() {
-    game("scissors")
-    
-})
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
 }
 
 main();
+
 
 
 
